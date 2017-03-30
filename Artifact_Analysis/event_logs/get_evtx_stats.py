8b077de27984fd07f62cfa6818353785da350a1c
Glenn <hiddenillusion@gmail.com>
2017-03-30 12:59:55 -0400

    updated get_evtx_stats.py script

--- Artifact_Analysis/event_logs/get_evtx_stats.py
+++ Artifact_Analysis/event_logs/get_evtx_stats.py
@@ -6,31 +6,38 @@
 # Date: 2016-03-30
 # (while at FireEye)
 
+from collections import OrderedDict
+from datetime import datetime
 import os
 import sys
-from datetime import datetime
-from collections import OrderedDict
 
 def doWork(filepath):
-    with open(filepath, "rb") as fobj:
-        evtx_file = pyevtx.file()
-        evtx_file.open_file_object(fobj)
+	with open(filepath, "rb") as fobj:
+		evtx_file = pyevtx.file()
+		evtx_file.open_file_object(fobj)
 
-        details = OrderedDict()
-        evtx_log_timestamps = []
+		details = OrderedDict()
+		total_records = evtx_file.get_number_of_records()
+		if not total_records > 0:
+			first_record = "N/A"
+			last_record = "N/A"
+		else:
+			evtx_log_timestamps = []
+			for record_index, evtx_record in enumerate(evtx_file.records):
+				# Don't want any 1601-01-01 00:00:00
+				if not evtx_record.get_written_time_as_integer() == 0:
+					evtx_log_timestamps.append(evtx_record.get_written_time())
 
-        for record_index, evtx_record in enumerate(evtx_file.records):
-            # Don't want any 1601-01-01 00:00:00
-            if not evtx_record.get_written_time_as_integer() == 0:
-                evtx_log_timestamps.append(evtx_record.get_written_time())
+			summarized = sorted(evtx_log_timestamps, key=lambda ts: datetime)
+			first_record = min(summarized)
+			last_record = max(summarized)
+					
+		details['first_record'] = first_record
+		details['last_record'] = last_record
+		details['total_records'] = total_records
+		details['evtx_filepath'] = os.path.abspath(filepath) 
 
-        summarized = sorted(evtx_log_timestamps, key=lambda ts: datetime)
-        details['first_record'] = min(summarized)
-        details['last_record'] = max(summarized)
-        details['total_records'] = evtx_file.get_number_of_records()
-        details['evtx_filepath'] = os.path.abspath(filepath)
-
-        return details
+		return details
 
 def recurse_path(path):
 	if os.path.isdir(path):
@@ -43,22 +50,22 @@ def recurse_path(path):
 		yield path
 
 if __name__ == '__main__':
-    import argparse
-    try:
-      import pyevtx
-    except ImportError:
-      print("[!] pyevtx required : https://github.com/libyal/libevtx/wiki/Building")
-      sys.exit(1)
+	import argparse
+	try:
+		import pyevtx
+	except ImportError:
+		print("[!] pyevtx required : https://github.com/libyal/libevtx/wiki/Building")
+		sys.exit(1)
 
-    parser = argparse.ArgumentParser(description='Provides summary details for Windows EVTX Event Logs')
-    parser.add_argument('-i', '--input', action='store', help='Path to EVTX file(s)', required=True)
-    args = vars(parser.parse_args())
+	parser = argparse.ArgumentParser(description='Provides summary details for Windows EVTX Event Logs')
+	parser.add_argument('-i', '--input', action='store', help='Path to EVTX file(s)', required=True)
+	args = vars(parser.parse_args())
 
-    path = args['input']
-    cnt = 0
-    for filepath in recurse_path(path):
-        evtx_details = doWork(filepath)
-        if cnt == 0:
-            print('\t'.join('{0}'.format(k) for k,v in evtx_details.iteritems()))
+	path = args['input']
+	cnt = 0
+	for filepath in recurse_path(path):
+		evtx_details = doWork(filepath)
+		if cnt == 0:
+			print('\t'.join('{0}'.format(k) for k,v in evtx_details.iteritems()))
 
-        print('\t'.join('{0}'.format(v) for k,v in evtx_details.iteritems()))
+		print('\t'.join('{0}'.format(v) for k,v in evtx_details.iteritems()))
