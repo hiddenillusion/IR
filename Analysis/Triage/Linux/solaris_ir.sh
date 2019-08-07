6d920314d02d9b94321f2a0340973b8541fa048f
Justin Case <hiddenillusion@gmail.com>
2019-08-07 21:12:47 +0000

    Updated Solaris IR script to handle newer versions & create fs listing

--- Analysis/Triage/Linux/solaris_ir.sh
+++ Analysis/Triage/Linux/solaris_ir.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ####################################################
 # Solaris 10 Triage script.
@@ -9,14 +9,15 @@
 # (While at FireEye)
 ####################################################
 
-# Some of these commands need root perms 
+# Some of these commands need root perms
 
 Date=`date +%m-%d-%Y_%T`
 dirname=`hostname`
 if [ ! -d dirname ]; then
-	mkdir $dirname
+  mkdir $dirname
 fi
 
+# Change working directory to triage directory
 cd $dirname
 outty="$Date.txt"
 
@@ -29,62 +30,102 @@ echo "======================================================" >> $outty
 find / -name ".bash_history" | while read f; do cat $f > `echo $f | sed 's/\//_/g'`.txt;done
 
 cmds=("ps -eaf"
-	"w"
-	"who"
-	"isainfo -vk"
-	"prtconf -v"
-	"prtdiag -v"
-	"ndd -get /dev/ip ip_forwarding"
-	"ndd -get /dev/ip ip6_forwarding"
-	"netstat -an"
-	"netstat -arn"
-	"arp -an"
-	"rpcinfo -p 127.0.0.1"
-	"ps -efLZ"
-	"ptree -ac"
-	"cat /var/adm/messages"
-	"modinfo -w"
-	"showrev -p"
-	"df -k"
-	"du -a"
-	"last"
-	"hostname"
-	"id"
-	"uname -a"
-	"hostid"
-	"ifconfig -a"
-	"netstat -rn"
-	"cat /etc/group"
-	"mount"
-	"crontab -l"
-	);
+  "w"
+  "who"
+  "isainfo -vk"
+  "prtconf -v"
+  "prtdiag -v"
+  "ndd -get /dev/ip ip_forwarding"
+  "ndd -get /dev/ip ip6_forwarding"
+  "netstat -an"
+  "netstat -arn"
+  "arp -an"
+  "rpcinfo -p 127.0.0.1"
+  "ps -efLZ"
+  "ptree -ac"
+  "cat /var/adm/messages"
+  "modinfo -w"
+  "showrev -p"
+  "df -k"
+  "du -a"
+  "last"
+  "hostname"
+  "id"
+  "uname -a"
+  "hostid"
+  "ifconfig -a"
+  "netstat -rn"
+  "mount"
+  "crontab -l"
+);
 
-cmds2=("cat /etc/passwd"
-	"cat /var/log/authlog"
-	"cat /var/log/apache2/access.log"
-	"cat /var/log/apache2/error.log"
-	"cat /etc/syslog.conf"
-	"cat /etc/inetd.conf"
-	);
+gen_files=("/etc/passwd"
+  "/etc/group"
+  "/etc/syslog.conf"
+  "/etc/inetd.conf"
+);
 
-echo "[-] Running command set 1"
+log_files=("authlog"
+  "apache2/access.log"
+  "apache2/error.log"
+  "lastlog"
+  "messages"
+  "sulog"
+  "wtmp"
+  "utmp"
+);
+
+log_dirs=("acct"
+  "log"
+  "sa"
+);
+
 # Commands that are having their output put into the main file
+echo "[-] Running triage commands"
 for c in "${cmds[@]}"; do
-	echo "------------------------------------------------------" >> $outty
-	echo "$c" >> $outty
-	echo "------------------------------------------------------" >> $outty
-	$c >> $outty
+  echo "------------------------------------------------------" >> $outty
+  echo "$c" >> $outty
+  echo "------------------------------------------------------" >> $outty
+  $c >> $outty
 done
 
-echo "[-] Running command set 2"
 # Commands that are having their output put into their own file
-for c2 in "${cmds2[@]}"; do
-	echo "------------------------------------------------------" >> $outty
-	echo "$c2" >> $outty
-	echo "------------------------------------------------------" >> $outty
-	output=`echo $c2 | sed s'/[ \/]/_/g'`.txt
-	$c2 1> $output 2>>$outty
+echo '[-] Grabbing general files of interest'
+for gf in "${gen_files[@]}"; do
+  echo "------------------------------------------------------" >> $outty
+  echo "Attempting to grab: $gf" >> $outty
+  echo "------------------------------------------------------" >> $outty
+  output=`echo $gf | sed s'/[ \/]/_/g'`.txt
+  cat $gf 1> $output 2>>$outty
 done
+
+# Determine where we're looking
+if [ -d /var/adm/ ]; then
+  DIR="/var/adm/"
+else
+  DIR="/var/log/"
+fi
+
+echo "[-] Grabbing files of interest from: $DIR"
+for lf in "${log_files[@]}"; do
+  echo "------------------------------------------------------" >> $outty
+  echo "$DIR$lf" >> $outty
+  echo "------------------------------------------------------" >> $outty
+  cp -f --parents $DIR$lf* . 2>>$outty
+  #output=`echo $DIRlf | sed s'/[ \/]/_/g'`.txt
+  #cat $DIR$lf 1> $output 2>>$outty
+done
+
+for ld in "${log_dirs[@]}"; do
+  echo "------------------------------------------------------" >> $outty
+  echo "$DIR$ld" >> $outty
+  echo "------------------------------------------------------" >> $outty
+  cp -rf --parents $DIR$ld . 2>>$outty
+done
+
+echo "[-] Generating file listing"
+find / 2>>$outty | while read fpath; do stat -c "%n %F %s %A %u %U %g %G %h %m %i %W %X %Y %Z"
+"$fpath" >> file_listing.txt; done
 
 echo "======================================================" >> $outty
 echo "[+] Collection Finished: $Date"
